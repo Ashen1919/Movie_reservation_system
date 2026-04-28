20f843602cf04fe8f126583b2eb6ef812c59c0bc
Ashen1919 <2303S0834@saegis.ac.lk>
2026-04-28 16:45:38 +0530

    Refactor myProfile service and controller: remove token dependency and directly use userId for fetching user profile

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,13 +2,20 @@ import prisma from "../../config/db.js";
 import { verifyRefreshToken } from "../../config/jwt.js"
 
 // get my profile service
-export const myProfile = async (token: string) => {
-    // get user ID from token
-    const payload = verifyRefreshToken(token) as { userId: string, role: string };
-
+export const myProfile = async (userId: string) => {
     // check user is exist
-    const user = await prisma.user.findUnique({ where: {id: payload.userId} });
-    if(!user || !user.refreshToken) throw new Error('User does not exist!');
+    const user = await prisma.user.findUnique({ 
+        where: {id: userId},
+        select: {
+            id: true,
+            name: true,
+            email: true,
+            createdAt: true,
+            role: true,
+            isEmailVerified: true
+        } 
+    });
+    if(!user) throw new Error('User does not exist!');
 
     return user;
 }
