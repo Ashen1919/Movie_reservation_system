376fd713edc0d18cfa8c82f47c0f35dfea478023
Ashen1919 <2303s0834@saegis.ac.lk>
2026-05-23 23:00:01 +0530

    feat: add security groups module with ALB, ECS, RDS, and ElastiCache security groups, including outputs and variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,15 @@ module "vpc" {
 
 # module ecr
 module "ecr" {
-  source = "./modules/ecr"
+  source       = "./modules/ecr"
   project_name = var.project_name
   environment  = var.environment
+}
+
+# module security groups
+module "security_groups" {
+  source       = "./modules/security_groups"
+  project_name = var.project_name
+  environment  = var.environment
+  vpc_id       = module.vpc.vpc_id
 }
