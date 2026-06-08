76b2a6f4c87cf4cecd26b77fce9e3abc5916e365
Ashen1919 <2303s0834@saegis.ac.lk>
2026-06-08 16:45:43 +0530

    feat: remove ElastiCache module and update security groups for Valkey EC2 instances

--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -56,36 +56,19 @@ resource "aws_security_group" "ecs_sg" {
     tags = { Name = "${var.project_name}-${var.environment}-ecs-sg"}
 }
 
-# security group for RDS
-resource "aws_security_group" "rds_sg" {
-    name        = "${var.project_name}-${var.environment}-rds-sg"
-    description = "Security group for RDS"
+# security group for valkey ec2
+resource "aws_security_group" "valkey_ec2_sg" {
+    name        = "${var.project_name}-${var.environment}-valkey-ec2-sg"
+    description = "Security group for Valkey EC2 instances"
     vpc_id      = var.vpc_id
 
     ingress {
-        description = "Allow traffic from ECS security group"
-        from_port   = 5432
-        to_port     = 5432
+        description = "Allow SSH traffic from anywhere"
+        from_port   = 22
+        to_port     = 22
         protocol    = "tcp"
-        security_groups = [aws_security_group.ecs_sg.id]
+        cidr_blocks = [var.admin_ip_cidr]
     }
-
-    egress {
-        description = "Allow all outbound traffic"
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    tags = { Name = "${var.project_name}-${var.environment}-rds-sg"}
-}
-
-# security group for ElastiCache
-resource "aws_security_group" "elasticache_sg" {
-    name        = "${var.project_name}-${var.environment}-elasticache-sg"
-    description = "Security group for ElastiCache"
-    vpc_id      = var.vpc_id
 
     ingress {
         description = "Allow traffic from ECS security group"
@@ -103,5 +86,5 @@ resource "aws_security_group" "elasticache_sg" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    tags = { Name = "${var.project_name}-${var.environment}-elasticache-sg"}
+    tags = { Name = "${var.project_name}-${var.environment}-valkey-ec2-sg"}
 }
